328e3532a3b128480bc63436e7f46bf21d65fc68
Valkinsenn <valkinsenn@gmail.com>
2021-05-19 22:23:51 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// Clears the console:
+console.clear()
+
+// Grabs HTML DOM elements:
+const circleNumber = document.querySelector(".number h2")
+
+const circleStroke = document.querySelector(
+  ".percent-box svg circle:nth-child(2)"
+)
+
+// Setting the number for the counter:
+let percentNumber = 0
+
+const percentCounter = number => {
+  const counter = setInterval(() => {
+    circleNumber.innerHTML = `${percentNumber}<span>%</span>`
+
+    circleStroke.style.strokeDashoffset = `calc(440 - (440 * ${percentNumber}) / 100)`
+
+    if (number === percentNumber) {
+      clearInterval(counter)
+    } else {
+      percentNumber++
+    }
+  }, 10)
+}
+
+percentCounter(80)
